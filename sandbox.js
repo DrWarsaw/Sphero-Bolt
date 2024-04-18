21095648281feef7fe7d845636b1905cdfbc86f6
Ralph <02travis.ralph@gulllakecs.org>
2024-04-18 14:18:13 -0400

    Inserted Hello World From Wiki - Called and Stable

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,14 @@
 //Programmer: Travis Ralph
 //Team Member: Julian Granger
-//Bolt ID:
+//Bolt ID:183F
 //Date: 4.16.2024
 //Program: Hello World - Sandbox
 //Program url: https://edu.sphero.com/program/16700418/edit
 
 async function startProgram() {
-	// Write code here
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
 }
