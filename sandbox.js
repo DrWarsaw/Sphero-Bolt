7b7c64c316f2ba8a6a37cb7403f483100bbe2c77
Ralph <02travis.ralph@gulllakecs.org>
2024-04-19 14:15:09 -0400

    Personalizing Hello Square - Called and Stable

--- sandbox.js
+++ sandbox.js
@@ -19,19 +19,19 @@ async function startProgram() {
 
 	setMainLed({ r: 0, g: 0, b: 255 });
 
-	await speak("Hello Square", true);
+	await speak("Hello guys", true);
 
 	await delay(1);
 
-	for (var _i1 = 0; _i1 < 4; _i1++) {
+	for (var sq1 = 0; sq1 < 4; sq1++) {
 
 		setMainLed(getRandomColor());
 
 		await Sound.Game.Coin.play(true);
 
-		await roll((getHeading() + 90), 60, 1);
+		await roll((getHeading() + 90), 70, 1);
 
-		await delay(1);
+		await delay(0.02);
 
 	}
 
