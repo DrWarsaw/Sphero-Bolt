615aaf5dc4414cba34195e8d150715bce4622b20
Ralph <02travis.ralph@gulllakecs.org>
2024-04-26 13:42:42 -0400

    Julian Granger - Pc 21 Checkpoint One

--- obsticleCourse.js
+++ obsticleCourse.js
@@ -4,3 +4,13 @@
 //Date: 4.24.2024
 //Program: Obstical Course
 //Program url: https://edu.sphero.com/program/16700418/edit
+
+
+async function startProgram() {
+	// Julian Granger - Pc 21 Checkpoint One
+	await roll(0, 80, 1.5,);
+	await delay(1)
+	await roll((getHeading() + 90), 50, 1.5);
+	await delay(1)
+	await roll((getHeading() + 90), 50, 1);
+	await delay(1)
