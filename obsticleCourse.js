7d2d6057857ca941ff747fd4c580a9faf56e1e17
Ralph <02travis.ralph@gulllakecs.org>
2024-04-26 14:13:21 -0400

    Travis Ralph - Pc 20 Checkpoint Four

--- obsticleCourse.js
+++ obsticleCourse.js
@@ -12,14 +12,22 @@ async function startProgram() {
 	await delay(1)
 	await roll((getHeading() + 90), 50, 1.5);
 	await delay(1)
-	await roll((getHeading() + 90), 50, 1);
+	await roll((getHeading() + 90), 50, 1.25);
 	await delay(1)
-	
+
 	// Travis Ralph - Pc 20 Checkpoint Two
 	await roll((getHeading() - 90), 50, 1.5);
 	await delay(1)
-	await roll((getHeading() - 90), 50, 0.1);
+	await roll((getHeading() - 90), 50, 0.75);
 	await delay(1)
 
+	// Julian Granger - Pc 21 Checkpoint Three
+	await roll((getHeading() - 45), 60, 2);
+	await delay(1)
 	
+	// Travis Ralph - Pc 20 Checkpoint Four
+	await roll((getHeading() + 110), 60, 2);
+	await delay(1)
+	await roll (0, 60, 1);
 
+}
