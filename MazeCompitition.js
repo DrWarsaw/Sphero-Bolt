8266760d8d6dac6912c4afd8c63bea254b7181e0
Ralph <02travis.ralph@gulllakecs.org>
2024-04-30 13:28:56 -0400

    Beginnning Comments and Start of Program added

---
+++ MazeCompitition.js
@@ -0,0 +1,10 @@
+//Programmer: Travis Ralph
+//Team Member: Julian Granger
+//Bolt ID:183F
+//Date: 4.30.2024
+//Program: Maze Competition
+//Program url: 
+
+async function startProgram() {
+	// Write code here
+}
