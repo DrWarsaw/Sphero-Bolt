a76b64660d07ec02e553de93baf7e16c610594f7
Ralph <02travis.ralph@gulllakecs.org>
2024-04-30 14:09:19 -0400

    Laying Out Blueprints For Commits

--- MazeCompitition.js
+++ MazeCompitition.js
@@ -6,5 +6,36 @@
 //Program url: 
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Travis Ralph - Start Text and Audio Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Blue LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Sound One Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Red LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Sound Two Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Green LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Purple LED and Sound Three  Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Finish Text and Audio Commit - Date:
+
+
+
 }
